81c928122a45f4749b85c0aa9ef0c8355b29ce99
Chase Callahan <ctcallahan2@crimson.ua.edu>
2023-11-21 11:37:52 -0600

    added menu for part 2

--- nlp.py
+++ nlp.py
@@ -15,9 +15,33 @@ REVIEW_FILE_CLEANED = "review_data_cleaned.csv"
 
 def main():
     reviews_df = get_reviews_df()
-    print(reviews_df.describe())
-    print(reviews_df[["overall", "wordCount", "sentimentScore"]].describe())
+    
+    menu_choice = get_menu_choice()
+    while menu_choice != "5":
+        if(menu_choice == "1"):
+           print(reviews_df.describe())
+           print(reviews_df[["overall", "wordCount", "sentimentScore"]].describe())
+        elif(menu_choice == "2"):
+           ratings_by_month_analysis(reviews_df)
+        elif(menu_choice == "3"):
+           sentiment_type_distribution_across_ratings_analysis(reviews_df)
+        elif(menu_choice == "4"):
+           average_sentiment_score_across_ratings_analysis(reviews_df)
+        else:
+           print("Invalid!!!\n")
+        menu_choice = get_menu_choice()
 
+    print("Bye Bye!")
+
+def get_menu_choice():
+   print("1. General Overview")
+   print("2. Ratings By Month")
+   print("3. Sentiment Type disptribution Across Ratings")
+   print("4. Average Sentiment Score Across Ratings")
+   print("5. Exit")
+   return input("\nPlease choose an option:")
+
+def ratings_by_month_analysis(reviews_df: pd.DataFrame):
     monthly_reviews_df = pd.DataFrame({"month" : list(map(lambda x: x, range(1, 13)))})
     monthly_reviews_df.reset_index().set_index(["month"])
 
@@ -29,15 +53,15 @@ def main():
     
     monthly_reviews_df["monthName"] = [month_name[i] for i in monthly_reviews_df.index]
     
-    # plt.bar(x=monthly_reviews_df["monthName"], height=monthly_reviews_df["count"])
-    # plt.plot(monthly_reviews_df["monthName"], monthly_reviews_df["count"], color="red")
-    # plt.xticks(rotation=45)
-    # plt.title("Number of Reviews by Mont")
-    # plt.ylabel("Number of Reviews")
-    # plt.xlabel("Month")
-    # plt.show()
+    plt.bar(x=monthly_reviews_df["monthName"], height=monthly_reviews_df["count"])
+    plt.plot(monthly_reviews_df["monthName"], monthly_reviews_df["count"], color="red")
+    plt.xticks(rotation=45)
+    plt.title("Number of Reviews by Mont")
+    plt.ylabel("Number of Reviews")
+    plt.xlabel("Month")
+    plt.show()
 
-    print(reviews_df[["overall", "sentimentType"]])
+def sentiment_type_distribution_across_ratings_analysis(reviews_df: pd.DataFrame):
     sentiment_df = reviews_df.groupby(["overall", "sentimentType"]).size().to_frame("count").reset_index()
     sentiment_pivot = sentiment_df.pivot_table(index=["overall"], columns=["sentimentType"], values=["count"])
 
@@ -47,6 +71,17 @@ def main():
     plt.xlabel("Rating")
     plt.title("Sentiment Type Distribution Across Rating")
     plt.legend(["negative", "neutral", "positive"], title="sentiment_type")
+    plt.show()
+
+def average_sentiment_score_across_ratings_analysis(reviews_df: pd.DataFrame):
+    rating_sentiment_df = reviews_df.groupby(["overall"])["sentimentScore"].mean().reset_index()
+    rating_sentiment_df.rename(columns={"sentimentScore": "avgSentiment"}, inplace=True)
+    print(rating_sentiment_df)
+
+    plt.bar(x=rating_sentiment_df["overall"], height=rating_sentiment_df["avgSentiment"])
+    plt.title("Average Sentiment Score Across Ratings")
+    plt.xlabel("Rating")
+    plt.ylabel("Average Sentiment Score")
     plt.show()
 
 def get_reviews_df() -> pd.DataFrame:
