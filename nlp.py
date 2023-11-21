379363e66eeadf474cea33a6cc5c8db5bf11796a
Chase Callahan <ctcallahan2@crimson.ua.edu>
2023-11-21 08:54:43 -0600

    Reading in review data

--- nlp.py
+++ nlp.py
@@ -1,5 +1,25 @@
+import pandas as pd
+import gzip
+
+REVIEWS_FILE = "reviews_Office_Products_5.json.gz"
 
 def main():
-    pass
+    reviews_df = getDF(REVIEWS_FILE)
+    print(reviews_df)
+
+def parse(path):
+  g = gzip.open(path, 'rb')
+  for l in g:
+    yield eval(l)
+
+def getDF(path):
+  i = 0
+  df = {}
+  for d in parse(path):
+    df[i] = d
+    i += 1
+  return pd.DataFrame.from_dict(df, orient='index')
+
+
 
 main()
